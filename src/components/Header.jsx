685b4c8dea50477e5bbd32cc81fb97301fd13a4c
Alex Lee <1128alex@gmail.com>
2025-05-11 22:34:33 +0900

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,13 @@ const Header = () => {
                 <h1>Header</h1>
             </div>
             <div id="header-container">
-                <div className="logo-box">
-                    <a href="/" className="logo">
-                        <div className="name">Alex Lee</div>
-                        {/* <div className="school">University of Waterloo</div> */}
-                    </a>
-                </div>
-                <nav className="nav-box">
+                {/* <div className="logo-box"> */}
+                {/* <a href="/" className="logo"> */}
+                {/* <div className="name">Alex Lee</div> */}
+                {/* <div className="school">University of Waterloo</div> */}
+                {/* </a> */}
+                {/* </div> */}
+                <nav className="nav-box" style={{ position: "fixed" }}>
                     <ul className="nav">
                         <li className="item"><a href="#intro" className="link">intro</a></li>
                         <li className="item"><a href="#aboutme" className="link">aboutme</a></li>
