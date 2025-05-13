bfe65b5d7cd00ecd08076f1d0d93d5f662fde991
Alex Lee <1128alex@gmail.com>
2025-05-13 23:16:23 +0900

    starting about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
             <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <div style={{ width: "70%", float: "left" }}></div>
                 <div style={{ width: "30%", float: "left" }}>
-                    <img src={about} alt="" style={{ height: "100px" }} />
+                    <img src={about} alt="" />
                 </div>
             </div>
         </div>
