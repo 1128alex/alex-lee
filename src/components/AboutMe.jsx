81a838da0fa5cb503b2e6698b38db6421cac7d5b
Alex Lee <1128alex@gmail.com>
2025-05-14 22:45:18 +0900

    started about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,14 @@ const AboutMe = () => {
         <div>
             <h1>About Me</h1>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <div style={{ width: "70%", float: "left" }}></div>
+                <div style={{ width: "70%", float: "left" }}>
+                    <p>
+                        Hi! I'm Alex Lee, a software engineer based in Toronto, Canada. I am currently a student at the University of Waterloo, pursuing a degree in Computer Science. I have a passion for technology and love to learn new things. In my free time, I enjoy reading books, playing video games, and exploring the outdoors.
+                    </p>
+                    <p>
+                        I have experience in various programming languages and frameworks, including JavaScript, Python, React, and Node.js. I am always looking for new challenges and opportunities to grow my skills.
+                    </p>
+                </div>
                 <div style={{ width: "30%", float: "left" }}>
                     {/* <img src={about} alt="" /> */}
                     <img class="centered-and-cropped" width="520" height="600"
