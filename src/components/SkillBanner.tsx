1e556779db15855dbda8587a03a011a10b501211
Alex Lee <1128alex@gmail.com>
2025-05-16 12:54:00 +0900

    banner change

--- src/components/SkillBanner.tsx
+++ src/components/SkillBanner.tsx
@@ -10,15 +10,41 @@ const SkillBanner: React.FC<SkillBannerProps> = ({ name, logoPath }) => {
         <li
             style={{
                 marginBottom: "10px",
-                backgroundColor: "black",
+                position: "relative",
                 color: "white",
                 borderRadius: "5px",
                 padding: "5px 10px",
                 display: "inline-flex",
                 alignItems: "center",
                 fontSize: "18px",
+                overflow: "hidden",
+                background: "none",
             }}
         >
+            <span
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    background: "black",
+                    clipPath: "polygon(0 0, 100% 0, 0 100%)",
+                    zIndex: 0,
+                }}
+            />
+            <span
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    right: 0,
+                    width: "100%",
+                    height: "100%",
+                    background: "#121212",
+                    clipPath: "polygon(100% 0, 100% 100%, 0 100%)",
+                    zIndex: 0,
+                }}
+            />
             <img
                 src={logoPath}
                 alt={name}
@@ -27,9 +53,10 @@ const SkillBanner: React.FC<SkillBannerProps> = ({ name, logoPath }) => {
                     height: "22px",
                     marginRight: "5px",
                     objectFit: "contain",
+                    zIndex: 1,
                 }}
             />
-            {name}
+            <span style={{ zIndex: 1 }}>{name}</span>
         </li>
     );
 };
