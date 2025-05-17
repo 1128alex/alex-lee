6a94f52d98c5cd23d48cfe27117f15fda1ca99ef
Alex Lee <1128alex@gmail.com>
2025-05-18 01:42:02 +0900

    small changes

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
     return (
         <section>
             <div id="aboutme" style={{ paddingRight: "15%" }}>
-                <h1 style={{ color: "#609ca4", fontSize: "80px", paddingTop: "120px" }}>About Me</h1>
+                <h1 style={{ color: "#609ca4", fontSize: "80px", paddingTop: "90px" }}>About Me</h1>
                 <div style={{ display: "flex", justifyContent: "space-between" }}>
                     <div style={{ width: "70%", float: "left" }}>
                         <p style={{ color: "#99a9af", paddingTop: "10px", width: "90%", fontSize: "20px" }}>
@@ -15,7 +15,7 @@ const AboutMe = () => {
                             <h1 style={{ color: "#609ca4", fontSize: "30px", paddingTop: "20px" }}>Field of interest</h1>
                             <ul style={{ color: "#99a9af", fontSize: "18px", paddingLeft: "20px", listStyleType: "disc" }}>
                                 <li>Data Science/Data Analysis</li>
-                                <li>AI/ML/DL</li>
+                                <li>AI/ML</li>
                                 <li>Fullstack Development</li>
                                 <li>Game Development</li>
                             </ul>
@@ -23,12 +23,21 @@ const AboutMe = () => {
                         <div id="hobbies">
                             <h1 style={{ color: "#609ca4", fontSize: "35px", paddingTop: "20px" }}>Hobbies</h1>
                             <ul style={{ color: "#99a9af", fontSize: "18px", paddingLeft: "20px", listStyleType: "disc" }}>
-                                <li>Playing soccer</li>
+                                <li>Football (Soccer)</li>
                                 <li>Snowboarding</li>
                                 <li>Coding</li>
                                 <li>Playing video games</li>
                                 <li>Drawing</li>
                             </ul>
+                        </div>
+                        <div style={{ marginTop: "50px" }}>
+                            <a
+                                href={require("../assets/ResumeV10WW.pdf")}
+                                target="_blank"
+                                id="resume"
+                            >
+                                Resume
+                            </a>
                         </div>
                     </div>
                     <div style={{ width: "30%", float: "left" }}>
